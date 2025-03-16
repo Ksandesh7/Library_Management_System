caab21b6ac43b32feeb50ae9f41b9dc5d6a8a2ff
Ksandesh7 <117517950+Ksandesh7@users.noreply.github.com>
2025-03-17 00:42:25 +0530

    Solved bugs related to pageable

--- libraryManagementSystemBackend/src/main/java/com/tu/libraryManagementSystemBackend/repository/BookRepository.java
+++ libraryManagementSystemBackend/src/main/java/com/tu/libraryManagementSystemBackend/repository/BookRepository.java
@@ -2,11 +2,11 @@ package com.tu.libraryManagementSystemBackend.repository;
 
 import com.tu.libraryManagementSystemBackend.model.Book;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.awt.print.Pageable;
 import java.util.Optional;
 import java.util.UUID;
 
